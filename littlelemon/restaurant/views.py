76b1a2d87b7fba88d75049d04e6e8467abed32d2
Huan Ho (Bruce) <37372806+bruceho293@users.noreply.github.com>
2023-01-15 00:38:47 -0500

    week_2:add REST API with djangorestframework.

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from rest_framework import generics, viewsets
+
+from .serializers import BookingSerializer, MenuSerializer
+from .models import Menu, Booking
+
 # Create your views here.
 
 # def sayHello(request):
@@ -8,3 +13,16 @@ from django.shortcuts import render
 
 def index(request):
   return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+  queryset = Menu.objects.all()
+  serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Menu.objects.all()
+  serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+  queryset = Booking.objects.all()
+  serializer_class = BookingSerializer
+
